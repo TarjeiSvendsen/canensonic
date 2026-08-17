a9677e90d1cec05c6e3c9dcb4488acdbd494020a
Tarjei Svendsen <tarjeisvendsen@outlook.com>
2026-08-18 00:21:16 +0200

    feat: first commit
    added README and general project files.

---
+++ src/main/java/no/tari/canensonic/CanensonicApplication.java
@@ -0,0 +1,13 @@
+package no.tari.canensonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanensonicApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CanensonicApplication.class, args);
+    }
+
+}

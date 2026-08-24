98d6699858f90d8015c22447c69925fc1ff56681
Tarjei Svendsen <tarjeisvendsen@outlook.com>
2026-08-25 00:35:46 +0200

    feat: added the /rest/getOpenSubsonicExtensions endpoint.

--- src/main/java/no/tari/canensonic/api/system/SystemController.java
+++ src/main/java/no/tari/canensonic/api/system/SystemController.java
@@ -2,14 +2,13 @@ package no.tari.canensonic.api.system;
 
 import no.tari.canensonic.api.base.responses.SubsonicError;
 import no.tari.canensonic.api.base.responses.SubsonicRootElement;
+import no.tari.canensonic.utils.OpenSubsonicExtensionUtils;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tools.jackson.databind.ObjectMapper;
-
-import java.util.List;
 
 @RestController
 public class SystemController {
@@ -36,6 +35,33 @@ public class SystemController {
         SubsonicRootElement rootElement = new SubsonicRootElement();
         try {
             rootElement.setStatus("ok");
+            return new ResponseEntity<>(mapper.writeValueAsString(rootElement), HttpStatusCode.valueOf(200));
+        }catch (Exception e){
+            rootElement.setStatus("failed");
+            rootElement.addChildNode("error", SubsonicError.from(0));
+            return new ResponseEntity<>(mapper.writeValueAsString(rootElement), HttpStatusCode.valueOf(200));
+        }
+    }
+
+    // -- rest/getOpenSubsonicExtensions --
+
+    @GetMapping(value = {"/rest/getOpenSubsonicExtensions","/rest/getOpenSubsonicExtensions.view"},produces = "application/json")
+    ResponseEntity<String> openSubsonicExtensionsGet(){
+        return getOpenSubsonicExtensions();
+    }
+
+    @PostMapping(value = {"/rest/getOpenSubsonicExtensions","/rest/getOpenSubsonicExtensions.view"},produces = "application/json")
+    ResponseEntity<String> openSubsonicExtensionsPost(){
+        return getOpenSubsonicExtensions();
+    }
+
+    ResponseEntity<String> getOpenSubsonicExtensions(){
+        SubsonicRootElement rootElement = new SubsonicRootElement();
+        OpenSubsonicExtensionsElement extensionsElement = new OpenSubsonicExtensionsElement();
+        extensionsElement.setExtensions(OpenSubsonicExtensionUtils.getOpenSubsonicExtensions());
+        try {
+            rootElement.setStatus("ok");
+            rootElement.addChildNode("openSubsonicExtensions",extensionsElement);
             return new ResponseEntity<>(mapper.writeValueAsString(rootElement), HttpStatusCode.valueOf(200));
         }catch (Exception e){
             rootElement.setStatus("failed");
